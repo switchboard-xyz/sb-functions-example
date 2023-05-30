44b3ee8bd54ae1c11173c6dec460a8a011510d20
Mitch Gildenberg <mitch@switchboard.xyz>
2023-05-29 21:53:46 -0400

    demo init

---
+++ programs/demo/src/lib.rs
@@ -0,0 +1,38 @@
+pub mod sdk;
+pub use sdk::*;
+use anchor_lang::prelude::*;
+use solana_program::pubkey;
+use bytemuck;
+
+declare_id!("8kjszBCEgkzAsU6QySHSZvr9yFaboau2RnarCQFFvasS");
+
+#[derive(Default, Eq, PartialEq, Copy, Clone, AnchorSerialize, AnchorDeserialize, Debug)]
+pub struct BorshDecimal {
+    pub mantissa: i128,
+    pub scale: u32,
+}
+
+#[program]
+pub mod demo {
+    use super::*;
+
+    pub fn ping(ctx: Context<Ping>, params: PingParams) -> Result<()> {
+        // let func = ctx.accounts.function.load()?;
+        // msg!("{:#?}", params);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Ping<'info> {
+    /// CHECK: todo
+    #[account(constraint = validate_fn_quote(&function, &quote, &signer))]
+    pub function: AccountInfo<'info>,
+    /// CHECK: todo
+    pub quote: AccountInfo<'info>,
+    pub signer: Signer<'info>,
+}
+#[derive(Clone, AnchorSerialize, AnchorDeserialize, Debug)]
+pub struct PingParams {
+    pub value: BorshDecimal,
+}
